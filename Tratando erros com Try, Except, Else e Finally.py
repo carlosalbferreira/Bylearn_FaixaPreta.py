ca291c2cccd78ba4c1ccd2540c99167f01fa2c54
Carlos Alberto <83506328+carlosalbferreira@users.noreply.github.com>
2021-05-17 16:48:59 -0300

    Update Tratando erros com Try, Except, Else e Finally.py

--- Tratando erros com Try, Except, Else e Finally.py
+++ Tratando erros com Try, Except, Else e Finally.py
@@ -7,7 +7,22 @@ def divisao(a, b):
         return 'Número não pode ser dividido por ZERO'
 
 
-num1 = int(input('Digite um número para dividir : '))
-num2 = int(input('Digite outro número para dividir: '))
+num1 = input('Digite um número para dividir : ')
+num2 = input('Digite outro número para dividir: ')
+
+print(divisao(num1, num2))
+
+# Outra Forma de fazer a correção é:
+# Já informando os dois erros em uma só linha 
+
+def divisao(a, b):
+    try:
+        return int(a) / int(b)
+    except (ValueError, ZeroDivisionError):
+        return 'Ocorreu um problema verifique suas informações'
+
+
+num1 = input('Digite um número para dividir : ')
+num2 = input('Digite outro número para dividir: ')
 
 print(divisao(num1, num2))
