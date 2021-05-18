b060d0202b3fae01b54ca7e3bd3a3101290299fe
Carlos Alberto <83506328+carlosalbferreira@users.noreply.github.com>
2021-05-18 00:53:10 -0300

    Create numeros_com_random.py
    # escolhe 6 números aleatório de 1 á 60 como se escolhesse o numero da mega-sena

---
+++ numeros_com_random.py
@@ -0,0 +1,7 @@
+# randint() -> Gera valores pseudo-aleatórios entre os valores estabelecidos.
+from random import randint
+
+# Gerador de apostas para a mega-sena
+for i in range(6):
+    print(randint(1, 61), end=', ')
+ # escolhe 6 números aleatório de 1 á 60 como se escolhesse o numero da mega-sena
